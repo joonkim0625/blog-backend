927594e113e8be02ba417616dc3df63569b14b97
Hyunjoon <joonkim0625@gmail.com>
2020-06-30 19:01:58 -0500

    Add settings for sequelize

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -1,6 +1,13 @@
 module.exports = {
-  HOST: "ec2-34-197-188-147.compute-1.amazonaws.com",
-  USER: "cvasnbfexalnjy",
-  PASSWORD: "70f18f96ad761ff745c413398dc4039bef8e4a060842d57da470c84dc3b8b48a",
-  DB: "depjvbg41er2nm",
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "",
+  DB: "blog",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
