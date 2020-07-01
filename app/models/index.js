13aca288bfaf58e5dff7b5c6ec068b42af7ac4da
Hyunjoon <joonkim0625@gmail.com>
2020-06-30 22:58:33 -0500

    update server config

--- app/models/index.js
+++ app/models/index.js
@@ -1,16 +1,30 @@
 const dbConfig = require("../config/dbConfig");
+const env = require("../config/env");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+//const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//host: dbConfig.HOST,
+//dialect: dbConfig.dialect,
+//operatorsAliases: false,
+
+//pool: {
+//max: dbConfig.pool.max,
+//min: dbConfig.pool.min,
+//acquire: dbConfig.pool.acquire,
+//idle: dbConfig.pool.idle,
+//},
+//});
+
+const sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
   operatorsAliases: false,
 
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
+    max: env.max,
+    min: env.pool.min,
+    acquire: env.pool.acquire,
+    idle: env.pool.idle,
   },
 });
 
